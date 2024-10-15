c3fdbd0cfd4fe77073ce86ae0fb9f61e3f920338
abdul061 <godofcoc346346@gmail.com~>
2024-10-15 22:31:25 +0530

    first commit

--- src/Page/App.js
+++ src/Page/App.js
@@ -1,5 +1,4 @@
 import React, { useEffect } from "react";
-import { Route, Routes } from "react-router-dom";
 import Nav from "./components/Nav"; // Import your Nav component
 import Home from "./Index"; // Import your Home component
 import About from "./About"; // Import your About component
@@ -7,25 +6,27 @@ import Work from "./Work";
 import Footer from "./components/Footer";
 import Service from "./Service";
 import Contact from "./Contact";
-
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 // Import your Work component
 // Import your Services component
 // Import your Contact component
 
 const App = () => {
   return (
-    <div>
-      <Nav /> {/* Navigation component */}
+    <Router>
+      <div>
+        <Nav /> {/* Navigation component */}
 
-      <Routes>
-        <Route path="/" element={<Home />} /> {/* Home route */}
-        <Route path="/about" element={<About />} /> {/* About route */}
-        <Route path="/work" element={<Work />} />
-        <Route path="/service" element={<Service />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
-      <Footer />
-    </div>
+        <Routes>
+          <Route path="/" element={<Home />} /> {/* Home route */}
+          <Route path="/about" element={<About />} /> {/* About route */}
+          <Route path="/work" element={<Work />} />
+          <Route path="/service" element={<Service />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+        <Footer />
+      </div>
+      </Router>
   );
 };
 
